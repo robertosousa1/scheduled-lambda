b2b9906e8b7af5f78e69fe3bb4572af1626b2c7c
Roberto <roberto_junior_sousa@hotmail.com>
2020-05-02 00:50:16 -0300

    environment variables

---
+++ config/settings.js
@@ -0,0 +1,9 @@
+const env = require('env-var')
+
+const settings = {
+  NODE_ENV: env.get('NODE_ENV').required().asString(),
+  commitMessageUrl: env.get('APICommitMessageURL').required().asString(),
+  dbTableName: env.get('DbTableName').required().asString()
+}
+
+module.exports = settings
